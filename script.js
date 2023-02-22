3b0ee0c6941b2aa7d187ff7e6e4873267a035d81
murat <slipstream11@yandex.com>
2023-02-22 22:07:56 +0300

    DOM traversing

--- script.js
+++ script.js
@@ -57,31 +57,21 @@ btnScrollTo.addEventListener('click', function (e) {
 // 1.Add Event Listener to common parent element
 // 2.Determine what element originated the event
 
-// document.querySelector('.nav__links').addEventListener
-//   ('click', function (e) {
-//     e.preventDefault();
-//     console.log(e.target);
-
-//     //Matching strtegy
-//     if (e.target.classList.contains('nav__link')) {
-//       const id = e.target.getAttribute('href');
-//       console.log(id);
-//       document.querySelector(id)
-//         .scrollIntoView({ behavior: 'smooth' })
-//     }
-//   });
-
-
 document.querySelector('.nav__links').addEventListener
   ('click', function (e) {
     e.preventDefault();
     console.log(e.target);
+
+    //Matching strtegy
     if (e.target.classList.contains('nav__link')) {
       const id = e.target.getAttribute('href');
       console.log(id);
-      document.querySelector(id).scrollIntoView()
+      document.querySelector(id)
+        .scrollIntoView({ behavior: 'smooth' })
     }
-  })
+  });
+
+
 
 
 
@@ -256,6 +246,41 @@ document.querySelector('.nav').addEventListener(
   });
 
 */
+const h1 = document.querySelector('h1');
+
+///// Going Downwards: child--------------------------
+console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'yellow';
+
+///// Going Upwords: parents ----------------
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background =
+  'var(--gradient-primary)';
+
+
+/////// Going Sideways : Siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)'
+});
+
+
+
+
 
 
 
