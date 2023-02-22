fb998fc198ddf1032f1f771e1e5a431824b5ad98
murat <slipstream11@yandex.com>
2023-02-22 11:20:26 +0300

    attributes

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+/*
 //////////////////////////////////
 /////////////////////////////////
 //Selecting Elements---
@@ -38,14 +39,15 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
+*/
 const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// console.log(allSections);
 
 document.getElementById('section--1');
 //getElementsByTagName
 const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'));
 
@@ -53,13 +55,12 @@ console.log(document.getElementsByClassName('btn'));
 //.insertAdjacentHTML
 const message = document.createElement('div');
 message.classList.add('cookie-message');
-// message.textContent = 'We use cookies for improved functionality and analytics';
+
 message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
 // header.prepend(message);
 header.append(message);
 // header.append(message.cloneNode(true))
-
 // header.before(message);
 // header.after(message)
 
@@ -68,3 +69,85 @@ document.querySelector('.btn--close-cookie').
   addEventListener('click', function () {
     message.remove();
   });
+
+
+////////// Styles /////
+//// Styles ////  
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+
+message.style.height = Number.parseFloat
+  (getComputedStyle(message).height, 10) + 30 + 'px';
+console.log(getComputedStyle(message).height);
+//setProperty
+document.documentElement.style.setProperty
+  ('--color-primary', 'orangered');
+
+/////////
+//// Attributes  -----------
+const logo = document.querySelector('.nav__logo');
+console.log(logo.src);
+console.log(logo.getAttribute('src'))
+console.log(logo.className);
+
+logo.alt = 'new logo text';
+console.log(logo.alt);
+
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+console.log(logo.getAttribute('company'))
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j')
+logo.classList.remove('c', 'j')
+logo.classList.toggle('c')
+console.log(logo.classList.contains('c'));
+
+console.log(logo.classList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const favoriteAnimal = 'shark';
+// switch (favoriteAnimal) {
+//   case 'bobcat':
+//   case 'cat':
+//     console.log('Cats are great')
+//     break;
+//   case 'dog': {
+//     console.log('dogs are loud')
+//     break
+//   }
+//   case 'shark':
+//     console.log('good choice')
+//     break
+//   default: console.log('animal')
+// };
+
