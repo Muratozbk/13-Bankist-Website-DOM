4440920648c2f9c65f02ead152bcfbaa7899d959
murat <slipstream11@yandex.com>
2023-02-23 21:59:22 +0300

    Observer API

--- script.js
+++ script.js
@@ -130,13 +130,51 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 //   }
 // });
 
-// 196
+//////// Sticky Navigation Bar 
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords)
 
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > initialCoords.top)
+//     nav.classList.add('sticky'); else
+//     nav.classList.remove('sticky')
+// });
 
+///// Sticky Nav: Intersection Observer API IMPORTANT 
 
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry)
+//   })
+// };
 
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
 
+// const observer =
+//   new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
 
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect();
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting)
+    nav.classList.add('sticky');
+  else nav.classList.remove('sticky')
+};
+
+const headerObserver = new IntersectionObserver(stickyNav,
+  {
+    root: null, threshold: 0,
+    rootMargin: `-${navHeight.height}px`
+  });
+headerObserver.observe(header);
 
 /*
 //////////////////////////////////
