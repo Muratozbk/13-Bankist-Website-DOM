efcce268d62d849daf4ae826d054a66f40cb189e
murat <slipstream11@yandex.com>
2023-02-24 11:46:29 +0300

    revealing elements by scrolling

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect();
-console.log(navHeight);
+// console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -175,6 +175,37 @@ const headerObserver = new IntersectionObserver(stickyNav,
     rootMargin: `-${navHeight.height}px`
   });
 headerObserver.observe(header);
+
+//// Reveal Sections Scrolling
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+//////////////
+//////////////
+
+
+
+
+
+
+
 
 /*
 //////////////////////////////////
