2d6ebe148125376097942925e7859c553ffa4fb8
murat <slipstream11@yandex.com>
2023-02-25 12:38:21 +0300

    slider next and prew done

--- script.js
+++ script.js
@@ -230,33 +230,48 @@ const btnRight = document.querySelector('.slider__btn--right');
 
 let curSlide = 0;
 const maxSlide = slides.length;
-
 const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.4) translateX(-850px)';
+slider.style.transform = 'scale(0.5) ';
 slider.style.overflow = 'visible';
 
-slides.forEach((s, i) =>
-  s.style.transform = `translateX(${100 * i}%)`)
-// 0%, 100%, 200%, 300%
+//// slides.forEach((s, i) =>  // goToSlide(0) --
+////   s.style.transform = `translateX(${100 * i}%)`)
+//// 0 %, 100 %, 200 %, 300 %
 
 const goToSlide = function (slide) {
   slides.forEach((s, i) =>
     s.style.transform =
     `translateX(${100 * (i - slide)}%)`)
 };
+
+goToSlide(0)
+
 //Next Slide
-btnRight.addEventListener('click', function () {
+const nextSlide = function () {
   if (curSlide === maxSlide - 1) {
     curSlide = 0;
   } else {
     curSlide++;
   }
-
   goToSlide(curSlide);
-});
+};
+//Prev Slide
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else
+    curSlide--;
+  goToSlide(curSlide)
+}
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
 
 
-//// 
+
+
+
+
+////
 // const cards = document.querySelectorAll('.card');
 
 // const observer = new IntersectionObserver(entries => {
